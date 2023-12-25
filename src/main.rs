5eba50a5a8d487ab1fb93d03bbbd0d7e87c97e1b
root <root@Furkan>
2023-12-25 23:16:32 +0300

    Decode bencoded integers

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,15 @@ use std::{env, usize};
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     // If encoded_value starts with a digit, it's a number
-    if let Some((len, result)) = encoded_value.split_once(':') {
+    if let Some(n) = encoded_value
+        .strip_prefix('i')
+        .and_then(|rest| rest.split_once('e'))
+        .and_then(|(digits, _)| digits.parse::<i64>().ok())
+    {
+        return n.into();
+    } else if let Some((len, rest)) = encoded_value.split_once(':') {
         if let Ok(len) = len.parse::<usize>() {
-            return serde_json::Value::String(result[..len].to_string());
+            return serde_json::Value::String(rest[..len].to_string());
         }
     }
     panic!("Unhandled encoded value: {}", encoded_value)
